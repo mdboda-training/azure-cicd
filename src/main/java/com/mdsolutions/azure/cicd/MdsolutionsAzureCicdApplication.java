8395612d7992d9208f2332708d2d363966ef91f8
unknown <boda.gadhilinga@gmail.com>
2025-02-20 09:33:51 +0530

    Initial Commit

---
+++ src/main/java/com/mdsolutions/azure/cicd/MdsolutionsAzureCicdApplication.java
@@ -0,0 +1,13 @@
+package com.mdsolutions.azure.cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MdsolutionsAzureCicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MdsolutionsAzureCicdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mdsolutions/azure/cicd/MdsolutionsAzureCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mdsolutions.azure.cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MdsolutionsAzureCicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
